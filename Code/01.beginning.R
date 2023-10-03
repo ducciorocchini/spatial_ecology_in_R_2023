ed9682f5681afce934ca4a304544d43a24228139
Duccio Rocchini <ducciorocchini@gmail.com>
2023-10-03 17:04:05 +0200

    Create 01.beginning.R

---
+++ Code/01.beginning.R
@@ -0,0 +1,19 @@
+# Here I can write anything I want!!! 42 is the meaning of life univcerse and all!
+
+# R as a calculator
+2 + 3
+
+# Assign to an object
+zima <- 2 + 3
+zima
+
+duccio <- 5 + 3
+duccio
+
+final <- zima * duccio
+final
+
+final^2
+
+
+
