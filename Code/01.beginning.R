8684a349acfa7a692032c48dd56b56cf07dca89a
Duccio Rocchini <ducciorocchini@gmail.com>
2023-10-03 17:52:20 +0200

    Update 01.beginning.R

--- Code/01.beginning.R
+++ Code/01.beginning.R
@@ -15,5 +15,25 @@ final
 
 final^2
 
+# array
+sophi <- c(10, 20, 30, 50, 70) # microplastics 
+# fcuntions have parentheses and inside them there are arguments
+
+paula <- c(100, 500, 600, 1000, 2000) # people
+
+plot(paula, sophi)
+
+plot(paula, sophi, xlab="number of people", ylab="microplastics")
+
+
+people <- paula
+microplastics <- sophi
+
+plot(people, microplastics)
+plot(people, microplastics, pch=19)
+# https://www.google.com/search?client=ubuntu-sn&hs=yV6&sca_esv=570352775&channel=fs&sxsrf=AM9HkKknoSOcu32qjoErsqX4O1ILBOJX4w:1696347741672&q=point+symbols+in+R&tbm=isch&source=lnms&sa=X&ved=2ahUKEwia9brkm9qBAxVrQvEDHbEYDuMQ0pQJegQIChAB&biw=1760&bih=887&dpr=1.09#imgrc=lUw3nrgRKV8ynM
+
+plot(people, microplastics, pch=19, cex=2)
+plot(people, microplastics, pch=19, cex=2, col="blue")
 
 
