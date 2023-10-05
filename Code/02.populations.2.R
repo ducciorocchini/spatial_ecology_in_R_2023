7bd4585fdffdd4f514a3bdbc54cc35b063b1774c
Duccio Rocchini <ducciorocchini@gmail.com>
2023-10-05 17:58:49 +0200

    Create 02.populations.2.R

---
+++ Code/02.populations.2.R
@@ -0,0 +1,46 @@
+# Code related to population ecology
+
+# A package is needed for point pattern analysis
+install.packages("spatstat")
+library(spatstat)
+
+# let's use the bei data:
+# data description:
+# https://CRAN.R-project.org/package=spatstat
+
+bei
+
+# plotting the data
+plot(bei)
+
+# changing dimension - cex
+plot(bei, cex=.2)
+
+# changing the symbol - pch
+plot(bei, cex=.2, pch=19)
+
+# additional datasets
+bei.extra
+plot(bei.extra)
+
+# let's use only part of the dataset: elev
+plot(bei.extra$elev)
+elevation <- bei.extra$elev
+plot(elevation)
+
+# second method to select elements
+elevation2 <- bei.extra[[1]]
+plot(elevation2)
+
+
+
+
+
+
+
+
+
+
+
+
+
