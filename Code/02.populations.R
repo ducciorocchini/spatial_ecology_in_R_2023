b70059e5b4d7351de3e37e5f6cdffc615fa86b92
Duccio Rocchini <ducciorocchini@gmail.com>
2023-10-05 12:01:05 +0200

    Update 02.populations.R

--- Code/02.populations.R
+++ Code/02.populations.R
@@ -1,5 +1,6 @@
 # uinstall.pcakages("spatstat")
 library(spatstat)
+library(terra)
 
 bei 
 plot(bei) 
@@ -17,3 +18,17 @@ density_map <- density(bei)
 par(mfrow=c(2,1))
 plot(bei.extra[[1]])
 plot(density_map)
+
+bei.rast <- rast(bei.extra[[1]])
+density.rast <- rast(density_map)
+
+randompoints <- spatSample(bei.rast, 100, "random", as.points = TRUE)
+bei.points <- terra::extract(bei.rast, randompoints)
+density.points <- terra::extract(density.rast, randompoints)
+ 
+pointmaps <- data.frame(bei.points[1:2], density.points[2])
+
+names(pointmaps)
+
+attach(pointmaps)
+plot(lyr.1, lyr.1.1)
