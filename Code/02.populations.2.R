1405bc0e862ad79c753badad1d45f526ec4981e1
Duccio Rocchini <ducciorocchini@gmail.com>
2023-10-10 17:44:52 +0200

    Update 02.populations.2.R

--- Code/02.populations.2.R
+++ Code/02.populations.2.R
@@ -32,6 +32,38 @@ plot(elevation)
 elevation2 <- bei.extra[[1]]
 plot(elevation2)
 
+# passing from points to a countinuous surface
+densitymap <- density(bei)
+plot(densitymap)
+points(bei, cex=.2)
+
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)
+plot(densitymap, col=cl)
+
+clnew <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(densitymap, col=clnew)
+
+plot(bei.extra)
+
+elev <- bei.extra[[1]] # bei.extra$elev
+plot(elev)
+
+# multiframe
+par(mfrow=c(1,2))
+plot(densitymap)
+plot(elev)
+
+par(mfrow=c(2,1))
+plot(densitymap)
+plot(elev)
+
+par(mfrow=c(1,3))
+plot(bei)
+plot(densitymap)
+plot(elev)
 
 
 
