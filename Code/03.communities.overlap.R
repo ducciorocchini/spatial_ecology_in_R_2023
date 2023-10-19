62195c3f7e5b293f75f92966e22d750e030579fe
Duccio Rocchini <ducciorocchini@gmail.com>
2023-10-19 11:44:06 +0200

    Update 03.communities.overlap.R

--- Code/03.communities.overlap.R
+++ Code/03.communities.overlap.R
@@ -1,12 +1,21 @@
+# camera traps data
+# data from Kerinci-Seblat National Park in Sumatra, Indonesia (Ridout and Linkie, 2009)
+# Ridout MS, Linkie M (2009). Estimating overlap of daily activity patterns from camera trap data. 
+# Journal of Agricultural, Biological, and Environmental Statistics, 14(3), 322–337.
+
 install.packages("overlap")
 library(overlap)
 # https://cran.r-project.org/web/packages/overlap/vignettes/overlap.pdf
+
 
 # data
 data(kerinci)
 head(kerinci)
 
 # tiger
+# The unit of time is the day, so values range from 0 to 1. 
+# The package overlap works entirely in radians: fitting density curves uses trigonometric functions (sin, cos, tan),
+# so this speeds up simulations. The conversion is straightforward:
 timeRad <- kerinci$Time * 2 * pi
 timeRad
 tig <- timeRad[kerinci$Sps == 'tiger']
