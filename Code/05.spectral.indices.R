591150854aa0685a9653fb0ba48bcf652fbc44a9
Duccio Rocchini <ducciorocchini@gmail.com>
2023-11-09 17:14:41 +0100

    Update 05.spectral.indices.R

--- Code/05.spectral.indices.R
+++ Code/05.spectral.indices.R
@@ -12,7 +12,27 @@ im.plotRGB(m1992, 1, 2, 3)
 im.plotRGB(m1992, r=2, g=1, b=3)
 im.plotRGB(m1992, r=2, g=3, b=1)
 
-
 # import the recent image
 m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
 im.plotRGB(m2006, r=2, g=3, b=1)
+
+# build a multiframe with 1992 and 2006 images
+par(mfrow=c(1,2))
+im.plotRGB(m1992, r=2, g=3, b=1)
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+# DVI = NIR - RED
+# bands: 1=NIR, 2=RED, 3=GREEN
+
+dvi1992 = m1992[[1]] - m1992[[2]]
+plot(dvi1992)
+
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi1992, col=cl)
+
+# exercise: calculate dvi of 2006
+dvi2006 = m2006[[1]] - m2006[[2]]
+plot(dvi2006, col=cl)
+
+
+
